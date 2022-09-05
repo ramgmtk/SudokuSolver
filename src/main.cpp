e6b841d504bbb40c45563a448d6e24686dc762b6
ramgmtk <ramgmtk@gmail.com>
2022-09-05 14:52:34 -0700

    Initial setup for file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+void parseData(const std::string& data) {
+    //std::stringstream ss;
+    std::cout << data << std::endl;
+}
+
+int main(int argc, char** argv) {
+    std::ifstream infile;
+    std::string data;
+    if (argc < 2) throw std::invalid_argument("./main <input file>\n");
+    infile.open(argv[1]);
+    while(std::getline(infile, data)) {
+        parseData(data);
+    }
+    
     return 0;
 }
