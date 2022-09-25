0501f9448f4d135eef4bb5ff0fa539fe1c2e58d6
ramgmtk <ramgmtk@gmail.com>
2022-09-24 17:58:21 -0700

    Initial commit of solver function and utility functions

--- src/board.cpp
+++ src/board.cpp
@@ -7,6 +7,7 @@
 //Setup and initialize the starting array.
 //Dynamically allocate the stack structure.
 board::board(const std::string& numbers) {
+    this->initial_num = 0;
     this->size = 9;
     if (numbers.length() != this->size * this->size) 
         throw std::invalid_argument("Input file character total != 81\n");
@@ -37,13 +38,14 @@ board::board(const std::string& numbers) {
 
     for (int i = 0; i < this->size; i++) {
         for (int j = 0; j < this->size; j++) {
-            int num = this->arr[i][j];
-            if (num == 0) continue;
+            int value = this->arr[i][j];
+            if (value == 0) continue;
             int box = (i/3)*3 + j/3;
-            if (col_arr[j][num-1] || row_arr[i][num-1] || box_arr[box][num-1]) {
+            if (col_arr[j][value-1] || row_arr[i][value-1] || box_arr[box][value-1]) {
                 throw "Constructing an invalid board.";
             } else {
-                col_arr[j][num-1] = row_arr[i][num-1] = box_arr[box][num-1] = 1;
+                col_arr[j][value-1] = row_arr[i][value-1] = box_arr[box][value-1] = 1;
+                this->initial_num += 1;
             }
         }
     }
@@ -89,10 +91,63 @@ void board::print() {
 //Checks if the board is meets the condition for a sudoku solution.
 //called multiple times from the solver function.
 bool board::valid(number entry) {
-    if (col_arr[entry.col][entry.num-1] || row_arr[entry.row][entry.num-1] || box_arr[entry.box][entry.num-1]) {
+    if (this->col_arr[entry.col][entry.value-1] || this->row_arr[entry.row][entry.value-1] || this->box_arr[entry.box][entry.value-1])
         return false;
-    } else {
-        col_arr[entry.col][entry.num-1] = row_arr[entry.row][entry.num-1] = box_arr[entry.box][entry.num-1] = 1;
-        return true;
+    else return true;
+}
+
+void board::insert_num(number entry) {
+    if (this->valid(entry)) {
+        this->arr[entry.row][entry.col] = entry.value;
+        this->col_arr[entry.col][entry.value-1] = this->row_arr[entry.row][entry.value-1] = 
+            this->box_arr[entry.box][entry.value-1] = 1;
+    }
+    else throw std::domain_error("Not a valid location on board for number.\n");
+}
+
+void board::remove_num(number removal) {
+    this->arr[removal.row][removal.col] = 0;
+    this->row_arr[removal.row][removal.value - 1] = 0;
+    this->col_arr[removal.col][removal.value - 1] = 0;
+    this->box_arr[removal.box][removal.value - 1] = 0;
+}
+
+//Main function which will attempts to fill out the board.
+void board::solver() {
+    bool success = true;
+    int new_numbers = (this->size * this->size) - this->initial_num;
+    int i = 0;
+    int j = 0;
+    int n = 1;
+    for (int k = 0; k < new_numbers; k++) {
+        for (; i < this->size; i++) {
+            success = false;
+            for (; j < this->size; j++) {
+                for (; n < this->size+1; n++) {
+                    try {
+                        number num(k, i, j, ((i/3)*3 + j/3), n);
+                        this->insert_num(num);
+                        stack->insert_head(num);
+                        success = true;
+                        break;
+                    } catch (const std::domain_error) {
+                        continue;
+                    }
+                }
+                n = 1;
+                if (success) break;
+            }
+            j = 0;
+            if (success) break;
+        }
+        if (!success) {
+            if (stack->is_empty()) throw std::domain_error("No solution exists.\n");
+            number r_num = stack->delete_head();
+            k = r_num.num - 1;
+            i = r_num.row;
+            j = r_num.col;
+            n = r_num.value + 1;
+            this->remove_num(r_num);
+        } else i = 1; //is this necessary?
     }
 }
--- src/board.h
+++ src/board.h
@@ -11,18 +11,26 @@ class board {
         int **col_arr;
         int **row_arr;
         int **box_arr;
+        int initial_num;
         unsigned int size;
         struct number {
             unsigned int num;
             unsigned int row;
             unsigned int col;
             unsigned int box;
+            unsigned int value;
+            number(int n, int r, int c, int b, int v) : num(n), row(r), col(c), box(b), value(v) {}
+            number() {}
         };
         linked_list<number>* stack;
+    protected:
+        bool valid(number entry);
+        void insert_num(number entry);
+        void remove_num(number entry);
     public:
-        board() : size(9), stack(nullptr) {}
+        board() : size(9), initial_num(0), stack(nullptr) {}
         board(const std::string& numbers);
         ~board();
         void print();
-        bool valid(number entry);
+        void solver();
 };
